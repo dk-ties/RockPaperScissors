6d87f397af9dec7661775309698845987b2073ac
dk-ties <dk.ties@gmail.com>
2022-10-11 20:20:16 +0200

    function to get a random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("hello world");
+
+var getComputerChoiceArray = ["rock", "paper", "scissor"];
+
+function getComputerChoice() {
+  let randomChoice = Math.floor(Math.random() * getComputerChoiceArray.length);
+  let choice = getComputerChoiceArray[randomChoice];
+  /*    Between Rock
+            Paper
+            Scissor */
+  console.log(choice);
+}

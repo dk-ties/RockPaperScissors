bee0f2d587ee3cb26fc4b38c512ba1d5f45d25dc
dk-ties <dk.ties@gmail.com>
2022-10-11 21:06:39 +0200

    function round, comparing getComputerChoice and getPlayerChoice and return the winner

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 console.log("hello world");
 
 var getComputerChoiceArray = ["rock", "paper", "scissor"];
+let playerWin = "Hooray You win!";
+let computerWin = "DAMMMM The computer take this one";
 
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * getComputerChoiceArray.length);
@@ -9,6 +11,7 @@ function getComputerChoice() {
             Paper
             Scissor */
   /* console.log(choice); */
+  console.log(choice);
   return choice;
 }
 
@@ -18,5 +21,23 @@ function getPlayerChoice() {
 
   return playerChoice;
 }
-console.log(getComputerChoice());
-console.log(getPlayerChoice());
+/* console.log(getComputerChoice());
+console.log(getPlayerChoice()); */
+
+function round(getComputerChoice, getPlayerChoice) {
+  if (getComputerChoice === "rock" && getPlayerChoice != "paper") {
+    return computerWin;
+  } else if (getComputerChoice === "paper" && getPlayerChoice != "scissor") {
+    return computerWin;
+  } else if (getComputerChoice === "scissor" && getPlayerChoice != "rock") {
+    return computerWin;
+  } else {
+    return playerWin;
+  }
+}
+/*    else if getComputerChoice is  paper and getPlayerChoice is not scissor
+        return computerWin
+    else if getComputerChoice is  scissor and getPlayerChoice is not rock
+        return computerWin
+    else 
+        return playerWin*/

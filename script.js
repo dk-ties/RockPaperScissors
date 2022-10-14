d6a932ba6690780577a7bb9b97778b000437a15d
dk-ties <dk.ties@gmail.com>
2022-10-14 19:04:42 +0200

    dubugging switch state ment, fixed it Moved let into function, and use if else under each case

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ console.log("hello world");
 var getComputerChoiceArray = ["rock", "paper", "scissor"];
 let playerWin = "Hooray You win!";
 let computerWin = "DAMMMM The computer take this one";
-let computerChoice = getComputerChoice();
-let playerChoice = getPlayerChoice();
 
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * getComputerChoiceArray.length);
@@ -41,30 +39,39 @@ console.log(getPlayerChoice()); */
   console.log("So far so good");
 } */
 
-console.log(playerChoice);
-console.log(computerChoice);
+function round() {
+  let computerChoice = getComputerChoice();
+  let playerChoice = getPlayerChoice();
 
-function round(computerChoice, playerChoice) {
   switch (computerChoice) {
     case "rock":
-      if ((playerChoice = "rock")) {
-        if ((playerChoice = "paper")) {
-          console.log(playerWin);
-          break;
-        }
-        console.log(computerWin);
+      if (playerChoice === "rock") {
+        console.log("TieTieTie");
+      } else if (playerChoice === "paper") {
+        console.log(playerWin);
+      } else {
+        console.log("Computer took Rock " + computerWin);
       }
       break;
-    /*     case "paper":
-      if (playerChoice != "paper" || playerChoice != "scissor") {
-        console.log(computerWin);
+
+    case "paper":
+      if (playerChoice === "paper") {
+        console.log("TieTieTie");
+      } else if (playerChoice === "scissor") {
+        console.log(playerWin);
+      } else {
+        console.log("Computer took paper " + computerWin);
       }
       break;
+
     case "scissor":
-      if (playerChoice != "scissor" || playerChoice != "rock") {
-        console.log(computerWin);
-      } */
-    default:
-      console.log(playerWin);
+      if (playerChoice === "scissor") {
+        console.log("TieTieTie");
+      } else if (playerChoice === "rock") {
+        console.log(playerWin);
+      } else {
+        console.log("Computer took scissor " + computerWin);
+      }
+      break;
   }
 }
